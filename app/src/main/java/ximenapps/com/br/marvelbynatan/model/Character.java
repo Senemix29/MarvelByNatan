49d328270b9422bb4d1f952e8054946644139f3c
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2017-02-28 14:55:44 -0300

    Implements Parcelable interface methods at character model

--- app/src/main/java/ximenapps/com/br/marvelbynatan/model/Character.java
+++ app/src/main/java/ximenapps/com/br/marvelbynatan/model/Character.java
@@ -1,12 +1,31 @@
 package ximenapps.com.br.marvelbynatan.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Natan on 04/08/2016.
  */
-public class Character {
+public class Character implements Parcelable {
+    public static final Creator<Character> CREATOR = new Creator<Character>() {
+        @Override
+        public Character createFromParcel(Parcel in) {
+            return new Character(in);
+        }
+
+        @Override
+        public Character[] newArray(int size) {
+            return new Character[size];
+        }
+    };
     private String name;
     private String description;
     private Image thumbnail;
+
+    protected Character(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+    }
 
     public Image getThumbnail() {
         return thumbnail;
@@ -38,6 +57,17 @@ public class Character {
                 "name = " + name + '\n' +
                 "description = " + description + '\n' +
                 "thumbnail = " + thumbnail +
-                '}'+'\n';
+                '}' + '\n';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(description);
     }
 }
