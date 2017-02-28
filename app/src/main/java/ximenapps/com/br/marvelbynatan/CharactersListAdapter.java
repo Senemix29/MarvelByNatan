1dcc06f72fd248b637233dcc882429ced55fdfd5
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2017-02-28 14:57:34 -0300

    Set the FlexGrow property for each view item

--- app/src/main/java/ximenapps/com/br/marvelbynatan/CharactersListAdapter.java
+++ app/src/main/java/ximenapps/com/br/marvelbynatan/CharactersListAdapter.java
@@ -1,15 +1,13 @@
 package ximenapps.com.br.marvelbynatan;
 
 import android.content.Context;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import com.google.android.flexbox.FlexboxLayoutManager;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -20,8 +18,8 @@ import ximenapps.com.br.marvelbynatan.model.Character;
 
 public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAdapter.ViewHolder> implements ItemTouchHelperAdapter {
     private final OnStartDragListener dragStartListener;
-    List<Character> characters;
-    Context context;
+    private List<Character> characters;
+    private Context context;
 
     public CharactersListAdapter(List<Character> characters, Context context, OnStartDragListener dragStartListener) {
         this.characters = characters;
@@ -38,22 +36,16 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.characterName.setText(characters.get(position).getName());
         Picasso.with(context)
                 .load(characters.get(position).getThumbnail().getUrl())
-                .fit()
-                .centerCrop()
                 .into(holder.characterPicture);
-        holder.characterDrag.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (MotionEventCompat.getActionMasked(event) ==
-                        MotionEvent.ACTION_DOWN) {
-                    dragStartListener.onStartDrag(holder);
-                }
-                return false;
-            }
-        });
+
+        ViewGroup.LayoutParams lp = holder.characterPicture.getLayoutParams();
+        if (lp instanceof FlexboxLayoutManager.LayoutParams) {
+            FlexboxLayoutManager.LayoutParams flexboxLp = (FlexboxLayoutManager.LayoutParams)
+                    holder.characterPicture.getLayoutParams();
+            flexboxLp.setFlexGrow(1.0f);
+        }
     }
 
     @Override
@@ -75,14 +67,11 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView characterPicture, characterDrag;
-        TextView characterName;
+        ImageView characterPicture;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            characterName = (TextView) itemView.findViewById(R.id.character_name);
             characterPicture = (ImageView) itemView.findViewById(R.id.character_picture);
-            characterDrag = (ImageView) itemView.findViewById(R.id.character_drag);
         }
     }
 }
