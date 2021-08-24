3d13127babc1555319803bbff93c6f9722594013
Natan Ximenes <natanximenes29@gmail.com>
2021-08-24 15:07:20 -0300

    Adds api auth interceptor implementation

--- app/src/main/java/ximenapps/com/br/marvelbynatan/network/auth/ApiAuthenticatorInterceptor.kt
+++ app/src/main/java/ximenapps/com/br/marvelbynatan/network/auth/ApiAuthenticatorInterceptor.kt
@@ -5,62 +5,30 @@ import okhttp3.Request
 import okhttp3.Response
 import org.koin.core.component.KoinComponent
 import ximenapps.com.br.marvelbynatan.BuildConfig
+import ximenapps.com.br.marvelbynatan.BuildConfig.API_PRIVATE_KEY
+import ximenapps.com.br.marvelbynatan.BuildConfig.API_PUBLIC_KEY
 import ximenapps.com.br.marvelbynatan.network.exception.AuthException
 
-private const val ACCESS_TOKEN_HEADER = "x-access-token"
-private const val AUTH_HEADER = "Authorization"
-private const val AUTH_ENDPOINT = "challenge/api/login"
-private const val API_KEY_JSON_FIELD = "api_key"
+private const val TS_QUERY_PARAM_NAME = "ts"
+private const val API_KEY_QUERY_PARAM_NAME = "apikey"
+private const val HASH_QUERY_PARAM_NAME = "hask"
 
 class ApiAuthenticatorInterceptor : Interceptor, KoinComponent {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-
-        if (AuthMemoryCache.token.isEmpty()) {
-            authorizeUser(request, chain)
-        }
+        val hashKey = generateHash(timestamp, API_PRIVATE_KEY, API_PUBLIC_KEY)
+        val newUrl = request.url
+            .newBuilder()
+            .addQueryParameter(TS_QUERY_PARAM_NAME, timestamp)
+            .addQueryParameter(API_KEY_QUERY_PARAM_NAME, API_PUBLIC_KEY)
+            .addQueryParameter(HASH_QUERY_PARAM_NAME, hashKey)
+            .build()
 
         val ongoingRequest = request
             .newBuilder()
-            .addHeader(ACCESS_TOKEN_HEADER, AuthMemoryCache.token)
+            .url(newUrl)
             .build()
 
         return chain.proceed(ongoingRequest)
     }
-
-    private fun authorizeUser(request: Request, chain: Interceptor.Chain) {
-        val authRequest = createAuthorizationRequest(request)
-        val authRequestResponse = chain.proceed(authRequest)
-        val apiKey = getApiKeyFromResponse(authRequestResponse)
-        if (apiKey.isEmpty()) throw AuthException()
-
-        saveCurrentToken(apiKey)
-    }
-
-    private fun createAuthorizationRequest(request: Request): Request {
-        val credential = generateCredential()
-        return request.newBuilder()
-            .header(AUTH_HEADER, credential)
-            .url(BuildConfig.BASE_URL + AUTH_ENDPOINT)
-            .get()
-            .build()
-    }
-
-    private fun getApiKeyFromResponse(authRequestResponse: Response): String {
-        return try {
-            val responseBody = authRequestResponse.body?.string()
-//            JsonParser.parseString(responseBody).asJsonObject.get(API_KEY_JSON_FIELD).asString
-            ""
-        } catch (exception: Throwable) {
-            ""
-        }
-    }
-
-    private fun saveCurrentToken(token: String) {
-        AuthMemoryCache.token = token
-    }
-}
-
-private object AuthMemoryCache {
-    var token = ""
 }
--- app/src/main/java/ximenapps/com/br/marvelbynatan/network/auth/Credentials.kt
+++ app/src/main/java/ximenapps/com/br/marvelbynatan/network/auth/Credentials.kt
@@ -1,11 +1,23 @@
 package ximenapps.com.br.marvelbynatan.network.auth
 
-import android.util.Base64.NO_WRAP
-import android.util.Base64.encodeToString
+import java.security.MessageDigest
+import java.util.concurrent.TimeUnit
 
-private const val PREFIX = "Basic "
-fun generateCredential(username: String = "hello", password: String = "world") =
-    PREFIX + encodeToString(
-        "$username:$password".toByteArray(),
-        NO_WRAP
-    )
+private const val MD5 = "MD5"
+
+val timestamp = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()).toString()
+fun generateHash(vararg texts: String): String {
+    val concatenatedText = texts.joinToString(separator = "")
+    return md5(concatenatedText)
+}
+
+private fun md5(input: String): String {
+    val bytes = MessageDigest
+        .getInstance(MD5)
+        .digest(input.toByteArray())
+    return bytes.toHex()
+}
+
+private fun ByteArray.toHex(): String {
+    return joinToString("") { "%02x".format(it) }
+}
